c98040ffaeb49ab5618838c690a7cfe322f67f2e
Tushar-Shokeen <tusharshokeen32@gmail.com>
2022-07-11 12:39:06 +0530

    Updates

---
+++ precache-manifest.be2a3e5b8203a4e2c1ed614de535d026.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab7d7bdacab9b02aa17f8f44bbbe613",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Keeper-App/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "8c8bb7642c417719a1c8",
+    "url": "/Keeper-App/static/js/main.e4903f7c.chunk.js"
+  },
+  {
+    "revision": "660c3a2d9cc725da8ecd",
+    "url": "/Keeper-App/static/js/runtime-main.78711218.js"
+  }
+]);
